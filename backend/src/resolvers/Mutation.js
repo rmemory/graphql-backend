c15b6c7a8e0dec3fcbb303b7952b954379d0efe6
Richard Memory <richardfmemory@gmail.com>
2018-09-25 17:02:46 -0600

    Add initial resolvers

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -5,6 +5,18 @@ const Mutations = {
 		global.dogs.push(newDog);
 		return newDog;
 	},
+
+	async createItem(parent, args, ctx, info) {
+		// TODO: Authenticate user
+		const argsForPrisma = args.data;
+		const item = await ctx.db.mutation.createItem({
+			data: {
+				...argsForPrisma
+			},
+		}, info);
+
+		return item;
+	},
 };
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,8 +1,27 @@
+/* 
+ * If not authentication is required, simply forward the
+ * request directly to Prisma. 
+ * 
+ * const { forwardTo } = require('prisma-binding);
+ * 
+ * const Query = {
+ * 	myQuery (parent, args, ctx, info) {
+ * 		// where "thing" is an object defined in 
+ * 		// datamodel.qraphql
+ * 		things: forwardTo('db'),
+ * 	}
+ * }
+ */
+
 const Query = {
 	dogs(parent, args, ctx, info) {
 		global.dogs = global.dogs || [];
 		return global.dogs;
 	},
+	async items(parent, args, ctx, info) {
+		const items = await ctx.db.Query.items();
+		return items;
+	}
 };
 
 module.exports = Query;
